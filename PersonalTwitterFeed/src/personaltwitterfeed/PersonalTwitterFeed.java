cf68d0f1d3db30a12f3fade2f1654a3cfc68fb61
Miracle Howard <mahoward1@aggies.ncat.edu>
2018-09-19 15:26:16 -0400

    made new Tweet method

--- PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
+++ PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
@@ -17,7 +17,8 @@ import java.util.Scanner;
 public class PersonalTwitterFeed {
 
     private static int MAX_NUMBER_TWEETS = 200;
-    
+    static int numTweets = 0;
+    static Scanner keyboard = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
@@ -33,12 +34,10 @@ public class PersonalTwitterFeed {
         System.out.println("Nice to meet you " + tweeterName + "!");
         System.out.println("Enter your tweets and I will add them to your timeline!");
         
-        int numTweets = 0;
+       
         
         while(numTweets < (MAX_NUMBER_TWEETS - 1)) {
-            tweets[numTweets] = keyboard.nextLine()+ "\n" +getCurrentTimeStamp();
-            numTweets++;
-            
+            newTweet(tweets);
             
             System.out.println(tweeterName + "'s Personal Twitter Feed:");
             for(int i = 0; i < numTweets; i++) {
@@ -68,6 +67,11 @@ public class PersonalTwitterFeed {
         return simpleDate.format(new Date());
         
         
+    }
+    public static void newTweet(String [] tweets){
+    tweets[numTweets] = keyboard.nextLine()+ "\n" +getCurrentTimeStamp();
+            numTweets++;
+            
     }
     
 }
